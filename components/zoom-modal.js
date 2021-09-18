b5af924d93687b87601b019f208aeee0b79b213f
Ata Bayraktar <atabayraktar@Atas-MacBook-Pro.local>
2021-09-18 20:53:41 +0300

    zoom + responsive details + idil evleri

--- components/zoom-modal.js
+++ components/zoom-modal.js
@@ -1,5 +1,3 @@
-import { useEffect } from "react"
-
 export default function ZoomModal({ zoomRef, imageSrc }) {
     return (
         <div className="modal">
--- pages/postacilar-nilufer.js
+++ pages/postacilar-nilufer.js
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Header from '../components/detail-header'
-import { useRef } from 'react'
+import ZoomModal from '../components/zoom-modal'
+import { useState, useRef, useEffect } from 'react';
 
 export default function () {
+    const [modalSrc, setModalSrc] = useState("");
+    const [showModal, setShowModal] = useState(false);
+    const refx = useRef(null);
+    useOutsideAlerter(refx);
+
     const projectsSectionRef = useRef(null);
     const contactSectionRef = useRef(null);
     const aboutUsSectionRef = useRef(null)
+
     return (
         <>
+            {showModal && <ZoomModal zoomRef={refx} imageSrc={modalSrc} />}
             <Head>
                 <title>POSTACILAR | NİLÜFER</title>
                 <meta name="description" content="" />
@@ -19,7 +27,22 @@ export default function () {
                     <div className="col-12 col-md-5">
                         <div className="row">
                             <div className="detail-title">
-                                postacılar <span className="detail-title-stick"><div>|</div></span> NİLÜFER
+                                POSTACILAR <span className="detail-title-stick"><div>|</div></span> NİLÜFER
+                            </div>
+                            <div className="col-12 col-md-7 nilufer-right-col nilufer-img-component-mobile">
+                                <div className="row">
+                                    <div className="col-6 nilufer-left-image">
+                                        <img onClick={() => { setShowModal(true); setModalSrc("/nilufer/nilufer2.jpg") }} width="100%" height="auto" src="/nilufer/nilufer2.jpg" />
+                                    </div>
+                                    <div className="col-6">
+                                        <div className="nilufer-right-top-image">
+                                            <img onClick={() => { setShowModal(true); setModalSrc("/nilufer/nilufer1.jpg") }} width="100%" height="auto" src="/nilufer/nilufer1.jpg" />
+                                        </div>
+                                        <div>
+                                            <img onClick={() => { setShowModal(true); setModalSrc("/nilufer/nilufer3.jpg") }} width="80%" height="auto" src="/nilufer/nilufer3.jpg" />
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className="detail-sub-title">
                                 NİLÜFER<span className="detail-sub-title-stick"><div>|</div></span>Hakkında
@@ -38,32 +61,17 @@ export default function () {
                             </div>
                         </div>
                     </div>
-                    <div className="d-none d-md-block col-12 col-md-7 nilufer-right-col">
-                        <div className="row mb-5 mb-md-0">
+                    <div className="col-12 col-md-7 nilufer-right-col nilufer-img-component-desktop">
+                        <div className="row">
                             <div className="col-6 nilufer-left-image">
-                                <img width="60%" height="auto" src="/nilufer/nilufer2.jpg" />
+                                <img onClick={() => { setShowModal(true); setModalSrc("/nilufer/nilufer2.jpg") }} width="80%" height="auto" src="/nilufer/nilufer2.jpg" />
                             </div>
                             <div className="col-6">
                                 <div className="nilufer-right-top-image">
-                                    <img width="90%" height="auto" src="/nilufer/nilufer1.jpg" />
+                                    <img onClick={() => { setShowModal(true); setModalSrc("/nilufer/nilufer1.jpg") }} width="90%" height="auto" src="/nilufer/nilufer1.jpg" />
                                 </div>
-                                <div>
-                                    <img width="70%" height="auto" src="/nilufer/nilufer3.jpg" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="d-block d-md-none col-12 col-md-7 nilufer-right-col">
-                        <div className="row mb-5 mb-md-0">
-                            <div className="col-6 nilufer-left-image">
-                                <img width="100%" height="auto" src="/nilufer/nilufer2.jpg" />
-                            </div>
-                            <div className="col-6">
-                                <div className="nilufer-right-top-image">
-                                    <img width="100%" height="auto" src="/nilufer/nilufer1.jpg" />
-                                </div>
-                                <div>
-                                    <img width="80%" height="auto" src="/nilufer/nilufer3.jpg" />
+                                <div className="nilufer-right-bottom-image">
+                                    <img onClick={() => { setShowModal(true); setModalSrc("/nilufer/nilufer3.jpg") }} width="70%" height="auto" src="/nilufer/nilufer3.jpg" />
                                 </div>
                             </div>
                         </div>
@@ -72,4 +80,23 @@ export default function () {
             </div>
         </>
     )
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+            /**
+             * Alert if clicked on outside of element
+             */
+            function handleClickOutside(event) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setShowModal(false);
+                }
+            }
+
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                // Unbind the event listener on clean up
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
 }

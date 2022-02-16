3c46edd42ba0dc0aa14db79464166a712c6b78c2
Ata Bayraktar <atabayraktar@Atas-MacBook-Pro.local>
2022-02-16 19:52:49 +0300

    fix

---
+++ components/about-us.js
@@ -0,0 +1,39 @@
+export default function AboutUs({ aboutUsRef }) {
+
+    return (
+        <>
+            <div id="hakkimizda" ref={aboutUsRef} className="container">
+                <div className="desktop-components">
+                    <div className="component-container">
+                        <div className="row">
+                            <div className="col-5">
+                                <div className="about-us-title">
+                                    Hakkımızda
+                                </div>
+                                <div className="about-us-description">
+                                    "Lorem dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                </div>
+                            </div>
+                            <div className="col-7 img-container">
+                                <img src="/1.jpg" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mobile-components">
+                    <div className="component-container">
+                        <div className="about-us-title">
+                            Hakkımızda
+                        </div>
+                        <div className="img-container">
+                            <img src="/1.jpg" />
+                        </div>
+                        <div className="about-us-description">
+                            "Lorem dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ components/icons.js
@@ -0,0 +1,55 @@
+export default function Icons() {
+
+    return (
+        <>
+            <div className="container">
+                <div className="component-container">
+                    <div className="row icons">
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo">
+                                <img src="/logo-1.png" />
+                            </div>
+                        </div>
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo">
+                                <img src="/logo-2.png" />
+                            </div>
+                        </div>
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo">
+                                <img src="/logo-3.png" />
+                            </div>
+                        </div>
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo">
+                                <img src="/logo-4.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row icons-description">
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo-description">
+                                POSTACILAR <br/>GÜVENCESİYLE
+                            </div>
+                        </div>
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo-description">
+                                GÜVENİLİR İŞ <br/>ORTAKLARI
+                            </div>
+                        </div>
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo-description">
+                                BİRİNCİ SINIF<br/> MALZEME VE İŞCİLİK
+                            </div>
+                        </div>
+                        <div className="col-3 icons-centered">
+                            <div className="about-us-logo-description">
+                                FONKSİYONEL <br/> MEKANLAR
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

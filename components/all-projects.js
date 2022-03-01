486c65d31b484233b1fdcd04d0cb287c33292620
Orhan Bartu Kutlu <orhanbartukutlu@Orhans-MacBook-Air.local>
2022-03-01 19:44:08 +0300

    last features

--- components/all-projects.js
+++ components/all-projects.js
@@ -1,14 +1,13 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function AllProjects() {
+  const router = useRouter();
 
-    const router = useRouter();
-
-    return (
-        <>
-            <div className="container">
-                <div className="desktop-components">
+  return (
+    <>
+      <div className="container">
+        {/* <div className="desktop-components">
                     <div className="component-container-last-item">
                         <div className="about-us-title">
                             Projelerimiz
@@ -68,42 +67,64 @@ export default function AllProjects() {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="mobile-components">
-                    <div className="component-container-last-item">
-                        <div className="about-us-title">
-                            Projelerimiz
-                        </div>
-                        <div className="img-container fadding-photo">
-                            <img onClick={() => router.push("/postacilar-life")} src="/1.jpg" />
-                        </div>
-                        <div className="row">
-                            <div className="col-6 img-container-main-project fadding-photo">
-                                <img onClick={() => router.push("/postacilar-nova")} src="/nova/nova_slide_1.jpg" />
-                            </div>
-                            <div className="col-6 img-container-projects fadding-photo">
-                                <img onClick={() => router.push("/postacilar-prestige")} src="/prestige/prestige_slide_2.JPG" />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-6 img-container-projects fadding-photo">
-                                <img onClick={() => router.push("/postacilar-yakamoz")} src="/yakamoz1.jpg" />
-                            </div>
-                            <div className="col-6 img-container-projects fadding-photo">
-                                <img onClick={() => router.push("/postacilar-idil")} src="/idil1.jpg" />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-6 img-container-projects fadding-photo">
-                                <img onClick={() => router.push("/postacilar-nilufer")} src="/nilufer3.jpg" />
-                            </div>
-                            <div className="col-6 img-container-projects fadding-photo">
-                                <img onClick={() => router.push("/postacilar-lotus")} src="/lotus/lotus2.jpg" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </div> */}
+
+        <div className="">
+          <div className="component-container-last-item">
+            <div className="about-us-title">Projelerimiz</div>
+            <div className="row">
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-life")}
+                  src="/life-cropped.jpeg"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-nova")}
+                  src="/nova/nova_slide_1.jpg"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-prestige")}
+                  src="/prestige/prestige_slide_2.JPG"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-yakamoz")}
+                  src="/yakamoz1.jpg"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-idil")}
+                  src="/idil1.jpg"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-nilufer")}
+                  src="/nilufer3.jpg"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <img
+                  onClick={() => router.push("/postacilar-lotus")}
+                  src="/lotus/lotus2.jpg"
+                />
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <div className="empty-image"></div>
+              </div>
+              <div className="col-md-4 col-6 img-container-projects fadding-photo">
+                <div className="empty-image"></div>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

fea21a110cbd03618cb4228cef89028c462821f0
atabayraktar <atabayraktar17@gmail.com>
2026-06-08 23:32:32 +0300

    scroll animation updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.scss';
 import Head from 'next/head';
@@ -9,6 +10,25 @@ import { AppProvider } from '../context/AppContext';
 import ScrollObserver from '../components/scroll-observer';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    let lenis;
+    import('lenis').then(({ default: Lenis }) => {
+      const isMobile = window.matchMedia('(max-width: 768px)').matches;
+      lenis = new Lenis({
+        lerp: 0.08,
+        smoothWheel: true,
+        smoothTouch: isMobile,
+        touchMultiplier: isMobile ? 2.5 : 2,
+      });
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+      requestAnimationFrame(raf);
+    });
+    return () => lenis?.destroy();
+  }, []);
+
   return (
     <AppProvider>
       <Head>
